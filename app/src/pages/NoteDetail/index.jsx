aacc7829ed46672da42c8612093980eed6e7959f
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-11-30 16:34:18 -0500

    [STYLE] add some styles in cart detail

--- app/src/pages/NoteDetail/index.jsx
+++ app/src/pages/NoteDetail/index.jsx
@@ -1,3 +1,4 @@
+import { Display, Text, User } from '@geist-ui/react'
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import useNotes from '../../hooks/useNotes'
@@ -7,6 +8,18 @@ export default function NoteDetail() {
 
   const { notes } = useNotes()
 
-  const noteDetail = notes.find((note) => note.id === id)
-  return <div>{noteDetail.content}</div>
+  const noteDetail = notes?.find((note) => note.id === id)
+
+  return (
+    <Display shadow width="500px">
+      <Text p>{noteDetail?.content}</Text>
+      <User
+        style={{ marginBottom: '1rem' }}
+        src="https://images.unsplash.com/photo-1579493934830-eab45746b51b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+        name={noteDetail?.user.name}
+      >
+        JavaScript engineer
+      </User>
+    </Display>
+  )
 }
